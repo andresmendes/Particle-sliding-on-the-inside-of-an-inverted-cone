876f7141bd5082dbaae2c1c097b8860cedd74bbd
André de Souza Mendes <asmendes@fei.edu.br>
2021-05-03 10:41:20 -0300

    Update particle_cone.m

--- particle_cone.m
+++ particle_cone.m
@@ -1,6 +1,6 @@
 %% Particle sliding on the inside of an inverted cone
-% Animation of a particle sliding with friction on the inside of an
-% inverted cone.
+% Simulation and animation of a particle sliding with friction on the
+% inside of an inverted cone.
 %
 %%
 
